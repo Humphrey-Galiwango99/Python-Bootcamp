dea10bbd2050d7d851462e78c994c500fe8d54aa
Humphrey-Galiwango99 <galiwangohumphrey55@gmail.com>
2021-07-20 13:26:37 +0300

    My first program in Python and Check on String Formating

---
+++ first.py
@@ -0,0 +1,11 @@
+#First line of Code
+print("Hello World")
+
+#Test on string formating
+firstName="Ddembe"
+lastName="Humphrey"
+Age=22
+Country="Uganda"
+Passion="Networking Engineer"
+
+print(f"Am {firstName} {lastName} from {Country}. Am {Age} years Old. i love to be a {Passion}")
